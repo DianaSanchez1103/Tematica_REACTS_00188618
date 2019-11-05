af0b02dac666ceb52647fddcdc4390afc8ac9840
DianaSanchez1103 <00188618@uca.edu.sv>
2019-11-05 02:02:52 -0600

    Updates

---
+++ precache-manifest.438eb6f3e12115f61179009a6aee0d87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3fa5f14c0afa2b4b2ffcede1f56f55d",
+    "url": "/Tematica_REACTS_00188618/index.html"
+  },
+  {
+    "revision": "9ee6e4db93bd2677ac17",
+    "url": "/Tematica_REACTS_00188618/static/css/main.318cce55.chunk.css"
+  },
+  {
+    "revision": "08c4d031c84967468e8e",
+    "url": "/Tematica_REACTS_00188618/static/js/2.0f6883a2.chunk.js"
+  },
+  {
+    "revision": "9ee6e4db93bd2677ac17",
+    "url": "/Tematica_REACTS_00188618/static/js/main.b12e796b.chunk.js"
+  },
+  {
+    "revision": "6b251d5a4a8e464c730b",
+    "url": "/Tematica_REACTS_00188618/static/js/runtime-main.c30d86b2.js"
+  }
+]);
